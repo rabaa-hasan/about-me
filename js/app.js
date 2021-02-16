dab386ca61c8d020bfb1b079ef138dfbeb0000f5
oebitw <engomarewies@gmail.com>
2021-02-16 10:47:51 +0200

    Update lab game

--- js/app.js
+++ js/app.js
@@ -8,64 +8,63 @@ alert('Nice meeting you ' + userName + '😎');
 let playGame = prompt('Are you Excited 🤩 ?  Do you want to play with us?');
 playGame = playGame.toLowerCase();
 //console.log(playGame);
-if(playGame==='yes'){
+if (playGame === 'yes' || playGame === 'y') {
   alert('You are on fire 🔥 let us start the game 😃 I will ask you 5 (yes or no) questions, & lets see how many points you will score.');
-} else{
+} else {
   alert('Duh You are boring 😪');
 }
 
-let backGround= prompt('Omar has Bsc degree in Aeronautical engineering ✈️ & Master degree in business administration 💰?');
+let backGround = prompt('Omar has Bsc degree in Aeronautical engineering ✈️ & Master degree in business administration 💰?');
 backGround = backGround.toLowerCase();
 //console.log(backGround);
-if(backGround==='yes'){
-  alert('Correct '+ userName +' You scored the first point 🎆');
-} else{
+if (backGround === 'yes' || backGround === 'y') {
+  alert('Correct ' + userName + ' You scored the first point 🎆');
+} else {
   alert('unfortunately wrong answer ⭕ ' + userName + ' 😔');
 }
 
 let workExperiance = prompt('Omar has experiance in business development & project management 🧑🏻‍💼?');
 workExperiance = workExperiance.toLowerCase();
 //console.log(workExperiance);
-if(workExperiance==='yes'){
-  alert('Well done '+ userName +' You scored the second point 🎆');
-} else{
+if (workExperiance === 'yes' || workExperiance === 'y') {
+  alert('Well done ' + userName + ' You scored the second point 🎆');
+} else {
   alert('unfortunately wrong answer ⭕ ' + userName + ' 😔');
 }
 
 let readBooks = prompt('Omar Loves reading books in his free time 📘?');
 readBooks = readBooks.toLowerCase();
 //console.log(readBooks);
-if(readBooks==='yes'){
-  alert('Correct '+ userName +' You scored the third point 🎆');
-} else{
+if (readBooks === 'yes' || readBooks === 'y') {
+  alert('Correct ' + userName + ' You scored the third point 🎆');
+} else {
   alert('unfortunately wrong answer ⭕ ' + userName + ' 😔');
 }
 
 let loveProgramming = prompt('Omar"s new hobby is programming 💻?');
 loveProgramming = loveProgramming.toLowerCase();
 //console.log(loveProgramming);
-if(loveProgramming==='yes'){
-  alert('Perfect '+ userName +' You scored the fourth point 🎆');
-} else{
+if (loveProgramming === 'yes' || loveProgramming === 'y') {
+  alert('Perfect ' + userName + ' You scored the fourth point 🎆');
+} else {
   alert('unfortunately wrong answer ⭕ ' + userName + ' 😔');
 }
 
 let favTeam = prompt('Omar"s favourite football team is barcelona 🔴🔵?');
 favTeam = favTeam.toLowerCase();
 //console.log(favTeam);
-if(favTeam==='yes'){
-  alert('Correct '+ userName +' You scored the fifth point 🎆');
-} else{
+if (favTeam === 'yes' || favTeam === 'y') {
+  alert('Correct ' + userName + ' You scored the fifth point 🎆');
+} else {
   alert('unfortunately wrong answer ⭕ ' + userName + ' 😔');
 }
-let gameResult= prompt('How many correct answers you have?');
+let gameResult = prompt('How many correct answers you have?');
 //console.log(gameResult);
-if(gameResult>=3){
+if (gameResult >= 3) {
   alert('Congrants ' + userName + ' you know omar well, Omar Looking forward to meeting you soon 😀');
 } else {
   alert('Omar is looking forward to meeting you' + userName + ' so you will know each other more 😀');
 }
-
 
 
 
